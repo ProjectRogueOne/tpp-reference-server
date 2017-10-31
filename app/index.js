1ef68451cba2d407c4a82506d7654738e8279bfa
robmckinnon <rob@movingflow>
2017-10-31 17:24:22 +0000

    Configure controller for /account-request-authorise-consent
    Require authorisation to call /account-request-authorise-consent endpoint.
    
    Use accountRequestAuthoriseConsent function to handle request.

--- app/index.js
+++ app/index.js
@@ -8,6 +8,7 @@ const { requireAuthorization } = require('./authorization');
 const { login } = require('./login');
 const { proxyMiddleware } = require('./proxy.js');
 const { OBAccountPaymentServiceProviders } = require('./ob-directory');
+const { accountRequestAuthoriseConsent } = require('./account-request-authorise-consent');
 
 const app = express();
 app.options('*', cors());
@@ -18,6 +19,10 @@ app.use('/login', login.authenticate);
 app.use('/logout', login.logout);
 app.all('/account-payment-service-provider-authorisation-servers', requireAuthorization);
 app.use('/account-payment-service-provider-authorisation-servers', OBAccountPaymentServiceProviders);
+
+app.all('/account-request-authorise-consent', requireAuthorization);
+app.post('/account-request-authorise-consent', accountRequestAuthoriseConsent);
+
 app.all('/open-banking/*', requireAuthorization);
 app.use('/open-banking', proxyMiddleware);
 app.use('/session/check', session.check);
