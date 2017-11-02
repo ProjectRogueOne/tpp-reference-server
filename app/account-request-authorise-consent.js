e85ed8427d67028e6ae0a9aa230c3980394e36e5
robmckinnon <rob@movingflow>
2017-11-02 10:03:47 +0000

    Log errors thrown from setup account request

--- app/account-request-authorise-consent.js
+++ app/account-request-authorise-consent.js
@@ -1,4 +1,5 @@
 const { setupAccountRequest } = require('./setup-account-request');
+const error = require('debug')('error');
 
 const accountRequestAuthoriseConsent = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -7,6 +8,7 @@ const accountRequestAuthoriseConsent = async (req, res) => {
     await setupAccountRequest(authorisationServerId);
     return res.status(204).send();
   } catch (err) {
+    error(err);
     const status = err.status ? err.status : 500;
     return res.status(status).send({ message: err.message });
   }
