27c3b12d1432f5a104efdd84fe224a71625a3a8b
Ezo Saleh <ezo@the-experimenters.com>
2017-11-03 12:55:31 +0000

    Test redirection url should always be configured.

--- test/authorise-consent/redirection.test.js
+++ test/authorise-consent/redirection.test.js
@@ -7,17 +7,28 @@ const fakeUrl = 'http://localhost:999/authorized';
 describe('Authorise Consent Redirection', () => {
   let redirection;
 
-  before(() => {
-    redirection = proxyquire('../../app/authorise-consent/redirection', {
-      'env-var': env.mock({
-        REGISTERED_REDIRECT_URL: fakeUrl,
-      }),
+  describe('url configured', () => {
+    it('returns redirection url', () => {
+      redirection = proxyquire('../../app/authorise-consent/redirection', {
+        'env-var': env.mock({
+          REGISTERED_REDIRECT_URL: fakeUrl,
+        }),
+      });
+
+      assert.equal(redirection.url, fakeUrl);
     });
   });
 
-  describe('url', () => {
-    it('exposes configured redirection url', () => {
-      assert.equal(redirection.url, fakeUrl);
+  describe('url missing', () => {
+    it('throws an error', () => {
+      try {
+        redirection = proxyquire('../../app/authorise-consent/redirection', {});
+      } catch (e) {
+        assert(
+          e.message.match(/"REGISTERED_REDIRECT_URL" is a required variable/),
+          'error message should indicate REGISTERED_REDIRECT_URL is missing',
+        );
+      }
     });
   });
 });
